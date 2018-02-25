24ed6323003a5ec10d6042bad2bf46b08378e625
Anton Zdanov <azdanov@protonmail.com>
2018-02-25 17:48:54 +0200

    feat: add home route
    Add '/' route to get information about microservice.
    Add promisified readFile to fetch a handlebars template.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = (req, res) => {
-  res.end('Welcome to Micro');
-};
+const { send } = require('micro');
+const { router, get } = require('microrouter');
+
+const home = require('./lib/home');
+
+const shorten = (req, res) =>
+  send(res, 200, `You've submitted ${req.params.linkToShorten}`);
+
+const fetch = (req, res) =>
+  send(res, 200, `Your id is ${req.params.fetchById}`);
+
+module.exports = router(
+  get('/', home),
+  get('/api/:linkToShorten', shorten),
+  get('/:fetchById', fetch),
+);
