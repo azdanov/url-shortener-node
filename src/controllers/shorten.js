2314a67e03c85efc9636f6544df0da8df4ecea4f
Anton Zdanov <azdanov@protonmail.com>
2018-02-27 14:58:43 +0200

    feat: add error message pages

--- src/controllers/shorten.js
+++ src/controllers/shorten.js
@@ -5,6 +5,7 @@ const Hashids = require('hashids');
 
 const hashids = new Hashids('', 6);
 const Url = require('../models/shorten');
+const createHomeView = require('../views/home');
 
 function createJsonResponse(url, req, id) {
   return {
@@ -13,36 +14,43 @@ function createJsonResponse(url, req, id) {
   };
 }
 
+function createResponse(url, req, foundDoc) {
+  const shortUrl = createJsonResponse(url, req, foundDoc.id);
+  return createHomeView(req, shortUrl);
+}
+
+function createErrorResponse(req, message) {
+  return createHomeView(req, { error: message });
+}
+
 const shorten = async (req, res) => {
-  const url = req.query.link;
+  const url = decodeURI(req.query.link);
   if (!url) {
-    send(res, 400, 'Wrong query parameter');
+    send(res, 400, await createErrorResponse(req, 'Invalid query parameter'));
     return;
   }
 
   try {
     const foundDoc = await Url.findOne({ url });
     if (foundDoc) {
-      const shortUrl = createJsonResponse(url, req, foundDoc.id);
-
-      send(res, 200, shortUrl);
+      const responsePage = await createResponse(url, req, foundDoc);
+      send(res, 200, responsePage);
       return;
     }
 
     const savedDoc = await Url({ url }).save();
     if (savedDoc) {
-      const shortUrl = createJsonResponse(url, req, savedDoc.id);
-
-      send(res, 201, shortUrl);
+      const responsePage = await createResponse(url, req, savedDoc);
+      send(res, 201, responsePage);
     }
   } catch (err) {
     if (err.name === 'ValidationError') {
       console.error('@shorten', err);
-      send(res, 412, `${err.name}: ${err.errors.url.message}`);
+      send(res, 412, await createErrorResponse(req, err.errors.url.message));
       return;
     }
     console.error('@shorten', err);
-    send(res, 500, 'Database unavailable');
+    send(res, 500, await createErrorResponse(req, 'Database unavailable'));
   }
 };
 
--- src/views/home.js
+++ src/views/home.js
@@ -6,12 +6,17 @@ const { URL } = require('url');
 
 const readFileAsync = promisify(fs.readFile);
 
-module.exports = async function createHomeView(req) {
+module.exports = async function createHomeView(req, payload = {}) {
   const source = await readFileAsync(
     path.resolve(__dirname, 'templates', 'home.hbs'),
     { encoding: 'utf8' },
   );
   const template = Handlebars.compile(source);
   const url = new URL(req.headers.host);
-  return template({ rootUrl: url });
+  return template({
+    root_url: url,
+    original_url: payload.original_url,
+    short_url: payload.short_url,
+    error: payload.error,
+  });
 };
